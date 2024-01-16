043d659c8e077308b764ed75cea97e00fb4c2b5d
Billy Rowden <williampsr.rowden@gmail.com>
2024-01-16 21:53:26 +0000

    added to the displayForecastedWeather function to display the temp, wind speed and humidity of the following 5 days

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 $(document).ready(function () { //ensures document is fully loaded and parsed before running code inside the function
   const apiKey = "4f2feff2957aa3c528f135007d7bca53"; // API Key
-  const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=4f2feff2957aa3c528f135007d7bca53'; // api end point url
+  const apiUrl = 'https://api.openweathermap.org/data/2.5/'; // api end point url
 
   // need a function to fetch the weather data from the OpenWeather API
   function getWeather(location) {
-    const currentWeatherUrl = `${apiUrl}weather?q=${city}&appid=${apiKey}&units=metric`;
-    const forecastedWeatherUrl = `${apiUrl}forecast?q=${city}&appid=${apiKey}&units=metric`;
+    const currentWeatherUrl = `${apiUrl}weather?q=${location}&appid=${apiKey}&units=metric`;
+    const forecastedWeatherUrl = `${apiUrl}forecast?q=${location}&appid=${apiKey}&units=metric`;
+
     // need to fetch the current weather data
     fetch(currentWeatherUrl)
       .then(response => response.json()) // parse the response from fetch request as JSON
@@ -20,7 +21,7 @@ $(document).ready(function () { //ensures document is fully loaded and parsed be
       .then(data => { 
         displayForecastedWeather(data); 
       })
-      .catch(error => console.error('Error fetching current weather', error));
+      .catch(error => console.error('Error fetching forecasted weather', error));
     }
 
   // need a function to display the current data
@@ -31,11 +32,30 @@ $(document).ready(function () { //ensures document is fully loaded and parsed be
                               <p>Wind: ${data.wind.speed} KPH</p>
                               <p>Humidity: ${data.main.humidity} %</p>`; // displays the name of the city, the country, temperature, wind speed and humidity.
   }
+
   // need a function to display the forecasted data
   function displayForecastedWeather(data) {
 
+    const forecastedSection = document.getElementById('forecast');
+    forecastedSection.innerHTML = ''; // clears the previous data
+
+    for (let i = 0; i < data.list.length; i++) {
+      const forecastItem = data.list[i];
+      const date = dayjs(forecastItem.dt_txt).format('YYYY-MM-DD HH:mm:ss');
+      const temperature = forecastItem.main.temp; // creates a variable to use later for data to be shown in forecasted section
+      const windSpeed = forecastItem.wind.speed;
+      const humidity = forecastItem.main.humidity;
+
+      forecastedSection.innerHTML += `<div class="col-md-2">
+                                      <p>Date: ${date}</p>
+                                      <p>Temperature: ${temperature} °C</p>
+                                      <p>Wind: ${windSpeed} KPH</p>
+                                      <p>Humidity: ${humidity} %</p>
+                                      </div>`;
+    }  
   }
-  getWeather('London');
+
+
   // need an event listener for the form submission
   // need to add each city searched to a list which represents the history of searched cities
 });
