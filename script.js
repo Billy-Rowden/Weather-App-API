d023b293b50a1538c0635ee27a736ed21a3e1d18
Billy Rowden <williampsr.rowden@gmail.com>
2024-01-16 17:44:59 +0000

    fixed html to link to bootstrap, added variables in js to hold my api key and end point

---
+++ script.js
@@ -0,0 +1,4 @@
+$(document).ready(function () { //ensures document is fully loaded and parsed before running code inside the function
+  const apiKey = "4f2feff2957aa3c528f135007d7bca53"; // API Key
+  const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=4f2feff2957aa3c528f135007d7bca53'; // api end point url
+})
