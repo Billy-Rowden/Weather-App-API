da8c756d29a7fa0ceaf9d012be8402e2e79a5333
Billy Rowden <williampsr.rowden@gmail.com>
2024-01-16 17:48:50 +0000

    added pseudo code to js file

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 $(document).ready(function () { //ensures document is fully loaded and parsed before running code inside the function
   const apiKey = "4f2feff2957aa3c528f135007d7bca53"; // API Key
   const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=4f2feff2957aa3c528f135007d7bca53'; // api end point url
+
+  // need a function to fetch the weather data from the OpenWeather API
+  // need to fetch the current weather data
+  // need to fetch the data for the next 5 days of forecast
+  // need a function to display the current data
+  // need a function to display the forecasted data
+  // need an event listener for the form submission
+  // need to add each city searched to a list which represents the history of searched cities
 })
